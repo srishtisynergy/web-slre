f6d5710c4a6fb2e7f971d594b7afd2647b3352f7
RohitShakyaDev <142961839+RohitShakyaDev@users.noreply.github.com>
2023-12-11 20:40:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // }
-  basePath: "",
-  output: "standalone",
-  experimental: {
-  appDir: true,
-},
+  output: "export",
+  images: {
+    unoptimized: true,
+  }
+//   basePath: "",
+//   output: "standalone",
+//   experimental: {
+//   appDir: true,
+// },
 };
 
 module.exports = nextConfig;
